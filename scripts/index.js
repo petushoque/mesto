5f878317cbde4f9742d030c814bc00e5770431ef
Alexei Kubasov <kubasovalexei@yandex.ru>
2020-12-21 18:54:40 +0700

    feat: доделать весь js, кнопка редактирования по прежнему летает, осталось сделать ее

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,49 @@ let openPopupButton = document.querySelector('.profile__edit-button')
 let overlay = document.querySelector('.overlay')
 let closePopupButton = document.querySelector('.popup__close-button')
 
+let nameEdit = document.querySelector('.popup__name-edit')
+let statusEdit = document.querySelector('.popup__status-edit')
+let profileName = document.querySelector('.profile__name')
+let profileStatus = document.querySelector('.profile__status')
+
+// === Функция открывающая попап ===
+
 openPopupButton.addEventListener('click', () => {
     overlay.classList.add('overlay_active')
+    nameEdit.value = `${profileName.textContent}` //значения в формах по умолчанию берем из HTML
+    statusEdit.value = `${profileStatus.textContent}` //значения в формах по умолчанию берем из HTML
 })
+
+// ===Функция закрывающая попап
 
 closePopupButton.addEventListener('click', () => {
     overlay.classList.remove('overlay_active')
 })
 
-
-// Находим форму в DOM
 let saveButton = document.querySelector('.popup__save-button')
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
+    evt.preventDefault(); 
+    let nameEditValue = document.querySelector('.popup__name-edit').value //берем текст в поле имя
+    let statusEditValue = document.querySelector('.popup__status-edit').value //берем текст в поле статус
+    profileName.innerHTML = `${nameEditValue}` //вставляем новые значения в HTML
+    profileStatus.innerHTML = `${statusEditValue}` //вставляем новые значения в HTML
+    overlay.classList.remove('overlay_active') //зыкраываем попап
+}
 
-    // Находим поля формы в DOM
+saveButton.addEventListener('click', handleFormSubmit); 
+
+
+
+/* ================ Запасная рабочая кнопка сохранить ===================
+
+let saveButton = document.querySelector('.popup__save-button')
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+
     let nameEdit = document.querySelector('.popup__name-edit').value
     let statusEdit = document.querySelector('.popup__status-edit').value
-
-    //nameEdit.value = '';
-    //statusEdit.value = '123'
 
     let profileName = document.querySelector('.profile__name')
     let profileStatus = document.querySelector('.profile__status')
@@ -35,6 +53,5 @@ function handleFormSubmit (evt) {
     profileStatus.innerHTML = `${statusEdit}`
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 saveButton.addEventListener('click', handleFormSubmit); 
+*/
